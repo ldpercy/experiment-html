ca1959e6061e74c50935925695a99cf14c5fe9c2
Paul Dale <ldpercy@gmail.com>
2025-03-17 19:15:25 +1100

    Split out util scripts

--- dynamic-load/util.js
+++ dynamic-load/util.js
@@ -1,27 +1,4 @@
 
-
-/* URL Parameters
-*/
-function getParameterByName(name)
-{
-	const url = window.location.href
-	name = name.replace(/[\[\]]/g, "\\$&")
-	const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)")
-	const results = regex.exec(url)
-	if (!results) return null
-	if (!results[2]) return ''
-	return decodeURIComponent(results[2].replace(/\+/g, " "))
-}
-
-
-
-
-function log(text) {
-	console.log(text);
-	document.getElementById('log').innerHTML += `${text}\n`;
-}
-
-
 /* addNewStylesheet
 as these appear later in the document they override styles before it
 */
---
+++ util/util.js
@@ -0,0 +1,21 @@
+
+/* URL Parameters
+*/
+function getParameterByName(name)
+{
+	const url = window.location.href
+	name = name.replace(/[\[\]]/g, "\\$&")
+	const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)")
+	const results = regex.exec(url)
+	if (!results) return null
+	if (!results[2]) return ''
+	return decodeURIComponent(results[2].replace(/\+/g, " "))
+}
+
+
+
+function log(text) {
+	console.log(text);
+	document.getElementById('log').innerHTML += `${text}\n`;
+}
+
