592f30ff7a8f97dd1a1e6071207ace03067d7c74
Paul Dale <ldpercy@gmail.com>
2025-03-17 19:19:30 +1100

    Copy out dynamic-load-css for splitting

---
+++ dynamic-load-css/dynamic-load-css.js
@@ -0,0 +1,46 @@
+
+function bodyOnload() {
+	log('body onload');
+
+	dynamicStylesheet = document.getElementById('dynamicStylesheet');
+	dynamicScript = document.getElementById('dynamicScript');
+
+	let folder = getParameterByName('folder');
+
+	log(folder);
+
+	let stylesheetUrl = `./${folder}/style.css`
+	log(stylesheetUrl);
+	// addNewStylesheet(stylesheetUrl)
+
+	dynamicStylesheet = document.getElementById('dynamicStylesheet');
+	switchStylesheet(dynamicStylesheet,stylesheetUrl)
+}
+
+
+/* addNewStylesheet
+as these appear later in the document they override styles before it
+*/
+function addNewStylesheet(url)
+{
+	let linkElement = document.createElement('link');
+	linkElement.rel = "stylesheet";
+	//linkElement.type = "text/css";
+	linkElement.href = url;
+	document.getElementsByTagName('head')[0].appendChild(linkElement)
+}
+
+
+/* switchStylesheet
+*/
+function switchStylesheet(linkElement, url) {
+	log(`${linkElement.id} ${url}`);
+	if 	(linkElement.nodeName == "LINK" && linkElement.rel == "stylesheet" )
+	{
+		linkElement.href = url;
+	}
+	else
+	{
+		log('Element not stylesheet link');
+	}
+}
---
+++ dynamic-load-css/dynamic-load-css.md
@@ -0,0 +1,10 @@
+Dynamic Load
+============
+
+Experiments in dynamically loading/changing styles and scripts.
+
+
+Styles
+------
+Updating the href attribute of a stylesheet link element works fine.
+
