96a94ce31e8748aa3a5d963c7e4b96c68bc13615
Paul Dale <ldpercy@gmail.com>
2025-05-29 09:29:16 +1000

    Tweaks for the template

--- [common]/common-script.js
+++ [common]/common-script.js
@@ -1,3 +1,4 @@
+console.log('common-script.js');
 
 /* URL Parameters
 */
--- [experiment-template]/experiment-template.js
+++ [experiment-template]/experiment-template.js
@@ -3,9 +3,8 @@
 console.clear();
 log = createPageLog(document.getElementById('pageLog'));
 log('experiment-template.js: run');
-
 document.addEventListener('DOMContentLoaded', documentDOMContentLoaded);
 
 function documentDOMContentLoaded() {
-	log('document DOMContentLoaded');
+	log('experiment-template.js: document.DOMContentLoaded');
 }
