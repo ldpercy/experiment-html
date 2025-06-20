6e48fb2b28abee548416541b032ba29978361089
Paul Dale <ldpercy@gmail.com>
2025-06-20 11:10:40 +1000

    Experimenting with css grid

---
+++ css-grid/css-grid.js
@@ -0,0 +1,10 @@
+/* css-grid.js
+*/
+console.clear();
+log = createPageLog(document.getElementById('pageLog'));
+log('css-grid.js: run');
+document.addEventListener('DOMContentLoaded', documentDOMContentLoaded);
+
+function documentDOMContentLoaded() {
+	log('css-grid.js: document.DOMContentLoaded');
+}
---
+++ css-grid/css-grid.md
@@ -0,0 +1,9 @@
+css-grid
+========
+
+
+https://css-tricks.com/snippets/css/complete-guide-grid/
+https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_grid_layout
+https://gridbyexample.com/examples/
+
+
