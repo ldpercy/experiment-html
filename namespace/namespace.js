66656a128b560ee94a06aeb54f72e1cd4085df0a
Paul Dale <ldpercy@gmail.com>
2025-10-19 19:21:18 +1100

    Experiment: namespace

---
+++ namespace/namespace.js
@@ -0,0 +1,63 @@
+/* namespace.js
+*/
+
+
+class NamespaceApp extends HTMLApp {
+	name = "namespaceApp";
+	info = "namespaceApp by ldpercy";
+
+	eventListeners = [
+		{
+			query: '#button-lightMode',
+			type: 'click',
+			listener: this.modeClickListener
+		},
+		{
+			query: '#button-darkMode',
+			type: 'click',
+			listener: this.modeClickListener
+		},
+		/* {
+			query: '#myForm',
+			type: 'change',
+			listener: this.formChangeListener
+		}, */
+	];
+
+
+
+
+	modeClickListener(event) {
+		//console.log(event.target.attributes); //attributes['value']
+		const newMode = event.target.attributes['mode'].value;
+		this.setMode(newMode);
+	}
+
+
+	setMode(mode) {
+		//console.log('setMode', mode);
+		if (mode === 'light')
+		{
+			document.documentElement.classList.replace('dark','light') ;
+		}
+		else {
+			document.documentElement.classList.replace('light','dark') ;
+		}
+		localStorage.mode = mode;
+	}
+
+
+	documentDOMContentLoaded() {
+		super.documentDOMContentLoaded();
+		this.setMode(localStorage.mode);
+
+	}
+
+
+}/* NamespaceApp */
+
+
+
+var namespaceApp = new NamespaceApp();
+
+
---
+++ namespace/namespace.md
@@ -0,0 +1,5 @@
+Namespace
+=========
+
+Having more ideas about using js classes as namespaces generally, want to try one or two things.
+
