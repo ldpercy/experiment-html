32f7dec9be42362f3b78bcc523411ce1092762b0
Paul Dale <ldpercy@gmail.com>
2026-01-26 19:22:25 +1100

    Add dedicated color scheme selector script & change a few pages to use it; + update all headers

---
+++ [common]/colourScheme-selector.js
@@ -0,0 +1,31 @@
+
+
+document.querySelectorAll('.colourScheme-selector').forEach(
+	(element) => {
+		element.addEventListener(
+			'click',
+			(event) => {
+				event.preventDefault();
+
+				const eventTarget = /** @type {HTMLElement} */ (event.target);		// this cast is awkward - see if there is a 'proper' way
+
+				this.setColourScheme(eventTarget.dataset.colourscheme);
+			}
+		);
+	}
+);
+
+
+function setColourScheme(schemeName) {
+	document.documentElement.style.setProperty('--colourScheme', schemeName);
+	document.documentElement.className = document.documentElement.className.replace(/(\bcolourScheme-\w+\b)?/,`colourScheme-${schemeName}`);
+}
+
+
+let colourScheme = getUrlParameter('colourScheme');
+
+
+if (colourScheme) {
+	setColourScheme(colourScheme);
+}
+
